874527913f7d4ff69b34453edbc44e2ec08f7241
GabrielCPDev <gabrielcpcontato@outlook.com>
2024-07-19 17:14:01 -0300

    metodos

--- composicao/pessoa.go
+++ composicao/pessoa.go
@@ -1,8 +1,24 @@
 package composicao
 
-import "goland/encapsulamento"
+import (
+	"goland/encapsulamento"
+	"time"
+)
 
 type Pessoa struct {
 	Nome     string
 	Endereco encapsulamento.Endereco
+	DataDeNacsimento time.Time
+}
+
+func (p Pessoa) IdadeAtual() int {
+	anoNascimento := p.DataDeNacsimento.Year()
+	anoAtual := time.Now().Year()
+	return anoAtual - anoNascimento
+}
+
+func CalculaIdade(p Pessoa) int {
+	anoNascimento := p.DataDeNacsimento.Year()
+	anoAtual := time.Now().Year()
+	return anoAtual - anoNascimento
 }
--- struct/struct.go
+++ struct/struct.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"goland/composicao"
 	"goland/encapsulamento"
+	"time"
 )
 
 func main() {
@@ -18,7 +19,15 @@ func main() {
 	pessoa := composicao.Pessoa {
 		Nome: "Oscar",
 		Endereco: endereco,
+		DataDeNacsimento: time.Date(1996, 06, 14, 0, 0, 0, 0, time.Local),
 	}
 
+
+	fmt.Println(endereco)
 	fmt.Println(pessoa)
+
+	idade := composicao.CalculaIdade(pessoa)
+	
+	idade = pessoa.IdadeAtual()
+	fmt.Println(idade)
 }
