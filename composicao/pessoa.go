13ccb1c1b1f43268b1e70fad8c898766ecfe09c4
GabrielCPDev <gabrielcpcontato@outlook.com>
2024-07-19 17:26:47 -0300

    alterando dados via metodos

--- composicao/pessoa.go
+++ composicao/pessoa.go
@@ -1,24 +1,21 @@
 package composicao
 
 import (
+	"fmt"
 	"goland/encapsulamento"
 	"time"
 )
 
 type Pessoa struct {
-	Nome     string
-	Endereco encapsulamento.Endereco
+	Nome             string
+	Endereco         encapsulamento.Endereco
 	DataDeNacsimento time.Time
+	Idade            int
 }
 
-func (p Pessoa) IdadeAtual() int {
+func (p *Pessoa) CalculaIdade()  {
 	anoNascimento := p.DataDeNacsimento.Year()
 	anoAtual := time.Now().Year()
-	return anoAtual - anoNascimento
-}
-
-func CalculaIdade(p Pessoa) int {
-	anoNascimento := p.DataDeNacsimento.Year()
-	anoAtual := time.Now().Year()
-	return anoAtual - anoNascimento
+	p.Idade = anoAtual - anoNascimento
+	fmt.Println("metodo calcula idade: ", p.Idade )
 }
--- struct/struct.go
+++ struct/struct.go
@@ -10,24 +10,21 @@ import (
 func main() {
 	fmt.Println("struct")
 
-	endereco := encapsulamento.Endereco {
-		Rua: "Rua x",
+	endereco := encapsulamento.Endereco{
+		Rua:    "Rua x",
 		Numero: 20,
 		Cidade: "Asgard",
 	}
-	
-	pessoa := composicao.Pessoa {
-		Nome: "Oscar",
-		Endereco: endereco,
+
+	pessoa := composicao.Pessoa{
+		Nome:             "Oscar",
+		Endereco:         endereco,
 		DataDeNacsimento: time.Date(1996, 06, 14, 0, 0, 0, 0, time.Local),
 	}
 
-
 	fmt.Println(endereco)
 	fmt.Println(pessoa)
-
-	idade := composicao.CalculaIdade(pessoa)
+	pessoa.CalculaIdade()
 	
-	idade = pessoa.IdadeAtual()
-	fmt.Println(idade)
+	fmt.Println("Idade: ", pessoa.Idade)
 }
