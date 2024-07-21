d09c95c7cb42a9f2baebae54f49eefe96f5e7043
GabrielCPDev <gabrielcpcontato@outlook.com>
2024-07-21 15:02:34 -0300

    channel buffer

--- goroutines/main.go
+++ goroutines/main.go
@@ -1,20 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 
-	channel := make(chan int)
+	channel := make(chan int, 100)
 	go setList(channel)
 
 	for valor := range channel {
-		fmt.Println(valor)
+		fmt.Println("recebendo: ", valor)
+		time.Sleep(time.Second)
 	}
 }
 
-func setList(channel chan int) {
+func setList(channel chan<- int) {
 	for i := 0; i < 100; i++ {
 		channel <- i
+		fmt.Println("enviando: ", i)
+
 	}
 	close(channel)
 }
